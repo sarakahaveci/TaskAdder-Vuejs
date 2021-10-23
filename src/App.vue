cd0109154809e81474769b62ce1b9c3a6040fa01
yamoonz <yamooonz@gmail.com>
2021-10-23 04:07:46 +0300

    added tasks

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
 <div class="container">
   <Header/>
+  <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks,
   },
   data(){
     return{
-      
+      tasks:[]
     }
+    },
+  created(){
+    this.tasks=[
+      {
+      "id": "1",
+      "text": "Doctors Appointment",
+      "day": "March 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": "2",
+      "text": "Meeting with boss",
+      "day": "March 6th at 1:30pm",
+      "reminder": false
+    },
+    {
+      "id": "3",
+      "text": "Food shopping",
+      "day": "March 7th at 2:00pm",
+      "reminder": true
+    }
+  ]
+}   
   }
-}
+
 </script>
 
 <style>
